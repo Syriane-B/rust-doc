0adf4e8de2a8c1037e172eecf69fe71da23fcdcf
Syriane Bonjean <syriane.bonjean@wizbii.com>
2026-01-25 12:25:49 +0100

    ownership examples

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("RUST");
+    let len = calculate_length(&s1);
+    println!("The length of '{}' is {}.", s1, len);
+
+    //s2 now hace the ownership
+    let s2 = s1;
+
+    // no longer exists
+    // println!("The length of '{}'.", s1);
 }
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+// Ownership
